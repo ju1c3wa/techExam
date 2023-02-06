dfaf0220252cfde455083b8dd48533f9919adef9
Joshua Balasabas <124347690+ju1c3wa@users.noreply.github.com>
2023-02-06 13:06:35 +0800

    Added assertion

--- cypress/e2e/addtocart.cy.js
+++ cypress/e2e/addtocart.cy.js
@@ -18,50 +18,41 @@ import Cart from "../pages/addtocart"
    })
 
    it("should Check Item Name and Prices", () => {
-      
-      cart.getOpenCartButton().click({force: true})
       cy.wait(2000)
+      cart.getOpenCartButton().click({force: true})
       cart.getCartDetailsPrice().then(($addPrice) => 
       {
       const cartAddedPrice = $addPrice.text()
       cy.log(cartAddedPrice)
       addToCartPrice = cartAddedPrice.parseInt
       })
-      cy.wait(2000)
       cart.getAddToCartItemName().then(($addItemName) =>
       {
         const addCartItemName = $addItemName.text()
         cy.log(addCartItemName)
         addToCartItemName = addCartItemName
       })
-      cy.wait(2000)
+      cy.wait(5000)
       cart.getAddCart().click()
-      cy.wait(1000)
+      cy.wait(2000)
       cart.getEnterLocation().type('Taguig, Metro Manila, Philippines')
-      cy.wait(5000)
       cart.getSelectLocation().click({force: true})
-      cy.wait(2000)
       cart.getConfirmLocation().click()
       cy.wait(5000)
       cart.getConfirmLocation().click()
-      cy.wait(2000)
+      cy.wait(1000)
       cart.getClickBarangayDropdown().click()
-      cy.wait(3000)
       cart.getSelectBarangay().click()
-      cy.wait(2000)
       cart.getEnterHouseAddress().type('Test')
-      cy.wait(2000)
       cart.getConfirmLocationButton().click()
       cy.wait(2000)
       cart.getOpenCartImageIcon().click()
-      cy.wait(2000)
       cart.getAddedToCartPrice().then(($inCartPrice) => 
       {
       const insideCartPrice = $inCartPrice.text()
       cy.log(insideCartPrice)
       cartPrice = insideCartPrice.parseInt
       })
-      cy.wait(2000)
       cart.getCartItemName().then(($cartItemName) => 
       {
       const insideCartItemName = $cartItemName.text()
@@ -69,8 +60,7 @@ import Cart from "../pages/addtocart"
       cartItemName = insideCartItemName
       })
 
-      assert(cartPrice == addToCartPrice, "Passed");
-      assert(addToCartItemName == cartItemName, "Passed");
+      cy.getCartDetailsPrice().should('have.value', addToCartPrice)
    })
   
  })
--- cypress/pages/addtocart.js
+++ cypress/pages/addtocart.js
@@ -15,6 +15,10 @@ class Cart {
   getCartDetailsPrice() {
     return cy.get('.srn-cart-details-price')
   }
+
+  getAddToCartItemName() {
+    return cy.get('.srn-cart-details-name')
+  }
   
   getAddCart() {
     return cy.get('.srn-button')
@@ -25,7 +29,7 @@ class Cart {
   }
 
   getSelectLocation() {
-    return cy.get('#ui-id-2')
+    return cy.get('#ui-id-2 ')
   }
 
   getConfirmLocation() {
@@ -33,35 +37,32 @@ class Cart {
   }
 
   getClickBarangayDropdown() {
-    return cy.get(':nth-child(6) > .dropdown > .srn-ft-input')
+    return cy.get(':nth-child(6) > .dropdown > .srn-ft-input' , { timeout: 10_000 })
   }
 
   getSelectBarangay() {
-    return cy.get(':nth-child(6) > .dropdown > .srn-ft-brgy-dropdown-menu > .srn-location-select-container > .srn-location-scrollable-list-container > .list-group > :nth-child(1)')
+    return cy.get(':nth-child(6) > .dropdown > .srn-ft-brgy-dropdown-menu > .srn-location-select-container > .srn-location-scrollable-list-container > .list-group > :nth-child(1)' , { timeout: 10_000 })
   }
 
   getEnterHouseAddress() {
-    return cy.get('#srn-ft-house_no-ft-modal-location')
+    return cy.get('#srn-ft-house_no-ft-modal-location' , { timeout: 10_000 })
   }
 
   getConfirmLocationButton() {
-    return cy.get('#Delivery > .srn-ft-proceed-btn')
+    return cy.get('#Delivery > .srn-ft-proceed-btn' , { timeout: 10_000 })
   }
 
   getOpenCartImageIcon() {
     return cy.get('a > img')
   }
   getAddedToCartPrice() {
-    return cy.get('.srnt-cart-item-price')
+    return cy.get('.srnt-cart-item-price', { timeout: 10_000 })
   }
 
   getCartItemName() {
-    return cy.get('.srnt-cart-item-title')
+    return cy.get('.srnt-cart-item-title', { timeout: 10_000 })
   }
 
-  getAddToCartItemName() {
-    return cy.get('.srn-cart-details-name')
-  }
 
 
 
